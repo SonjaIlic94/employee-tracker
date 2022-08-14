e3c5a96af3f34e33609b079bbc356ec5df72a8c1
sonjailic <sonjailic94@hotmail.com>
2022-08-14 15:24:36 -0400

    add roles table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(7,2),
+    department_id INTEGER
+);
+
+
 CREATE TABLE departments(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,8 @@
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Sales Representative', '55000.00', '1');
+
+
 INSERT INTO departments(name)
     VALUES
     ('Sales'),
