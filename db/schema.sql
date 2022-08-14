c2ba1bfef77e54b1306afe29bf96576cd61af80a
sonjailic <sonjailic94@hotmail.com>
2022-08-14 19:46:25 -0400

    updating tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,24 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE employee(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER
-    --constraint fk_manager FOREIGN KEY employee(id) REFERENCES employee(manager_id)
-)
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (roles_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)ON DELETE SET NULL
+);
 
 
-CREATE TABLE roles (
+CREATE TABLE roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(7,2),
-    department_id INTEGER
-    --constraint fk_role FOREIGN KEY role(department_id) REFERENCES employee(role_id) 
+    department_id INTEGER,
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 
@@ -21,6 +26,6 @@ CREATE TABLE departments(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
     -- constraint -- foreign key department ID references roles department_id
-    constraint fk_department FOREIGN KEY departments(id) REFERENCES roles(department_id) ON DELETE SET NULL
+   -- constraint fk_department FOREIGN KEY departments(id) REFERENCES roles(department_id) ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,12 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Tony', 'Hawk', 1, 0),
+('George', 'Clooney', 1, 2);
+
 INSERT INTO roles (title, salary, department_id)
 VALUES
-('Sales Representative', '55000.00', '1');
+('Sales Representative', '55000.00', 1);
+('Manager', '80000.00', 1);
 
 
 INSERT INTO departments(name)
