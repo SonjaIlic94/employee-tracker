1964eab9dab57ebc770f8bd7a1718ccfd14237e0
sonjailic <sonjailic94@hotmail.com>
2022-08-14 21:28:03 -0400

    start inquirer questions

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const inquirer = require('inquirer');
 
-// view all departments, 
+function promptUser() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee']
+        }
+    ]).then(response => {
+        if (response.menu === 'View all departments') {
+            console.log('you chose to view all depts...');
+        }
+        if (response.menu === 'View all roles') {
+            console.log('you chose to view all Roles...');
+        }
+        if (response.menu === 'View all employees') {
+            console.log('you chose to view all employees...');
+        }
+        if (response.menu === 'Add a department') {
+            console.log('you chose to add a dept...');
+        }
+        if (response.menu === 'Add a role') {
+            console.log('you chose to add a Role...');
+        }
+        if (response.menu === 'Add an employee') {
+            console.log('you chose to Add an employee...');
+        } else {
+            promptUser();
+        }
+    })
+};
+
+promptUser();
+// view all departments,
 //view all roles, 
-//iew all employees, 
+//view all employees, 
 //add a department, 
 //add a role, 
 //add an employee, 
